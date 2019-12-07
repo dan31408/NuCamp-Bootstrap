ff859001061eb297f225020e1862c22c930c4ec1
Dan Beto <dan.beto31408@gmail.com>
2019-12-07 13:51:55 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,16 @@
                         $("#carouselButton").children("i").removeClass("fa-play");
                         $("#carouselButton").children("i").addClass("fa-pause");
                     }
+                    
+                });
+
+                $("#loginButton").click(function() {
+                    $("#loginModal").modal('show');
+                });
+                
+                $("#reserveButton").click(function() {
+                    $("#reserveModal").modal('show');                           
                 });
             });
-        
+
+           
